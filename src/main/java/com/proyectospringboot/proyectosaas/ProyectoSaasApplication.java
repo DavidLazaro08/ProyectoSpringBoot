0b27597ccb69a828a7efacc427590cd365e4f5ea
David Gutiérrez <empirio14@alumnos.ilerna.com>
2026-02-05 17:27:55 +0100

    Semana 1: arranque proyecto Spring Boot

---
+++ src/main/java/com/proyectospringboot/proyectosaas/ProyectoSaasApplication.java
@@ -0,0 +1,13 @@
+package com.proyectospringboot.proyectosaas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoSaasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoSaasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/proyectospringboot/proyectosaas/ProyectoSaasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyectospringboot.proyectosaas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoSaasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
